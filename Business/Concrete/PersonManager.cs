a46984bb017fc28350eac6a18c982224f0584373
yasinbolat <106676214+ysnblt@users.noreply.github.com>
2024-02-07 17:47:11 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+     return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody
+     (person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,132 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskeTakip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Degiskenler();
+            Vatandas vatandas1 = new Vatandas();
+
+            SelamVer(isim: "Engin");
+            SelamVer(isim: "Ahmet");
+            SelamVer(isim: "Ayşe");
+            SelamVer();
+
+            int Sonuc = Topla(3, 5);
+            int Sonuc2 = Topla();
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+            //  ogrenciler[3] = "İlker";
+
+            ogrenciler = new string[4]; // Çalıştırıldığında yeni dizi algılar ve üçüncü elemana atama yapar diğerleri boş kalır.
+
+            ogrenciler[3] = "İlker";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "İstanbul", "Ankara", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            sayi2 = sayi1;
+            sayi1 = 30;
+
+            Console.WriteLine(sayi2);
+
+            Person person1 = new Person();
+            person1.FirstName = "Engin";
+            person1.LastName = "Demiroğ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            foreach (string sehir in sehirler1) 
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> sehirler1Yeni = new List<string>{ "Ankara", "İstanbul", "İzmir" };
+            sehirler1Yeni.Add("Adana");
+            foreach (var item in sehirler1Yeni)
+            {
+                Console.WriteLine(item);
+            }
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+
+
+
+
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam:" + sonuc);
+            return sonuc;
+        }
+
+
+
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Selam";
+            double tutar = 100000;
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demirog";
+            int dogumYili = 1985;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(mesaj);
+
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
+
